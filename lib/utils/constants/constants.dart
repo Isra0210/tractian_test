ab09b9f4d0ed716dc826b9bc42dfe71824109ae4
Israel Rodrigues <r.israel0210@gmail.com>
2024-10-18 23:44:27 -0300

    feat: add constants

---
+++ lib/utils/constants/constants.dart
@@ -0,0 +1,23 @@
+//Keys for api path
+const kAssets = 'assets';
+const kCompanies = 'companies';
+const kLocations = 'locations';
+
+//Keys for log
+const kAppLogs = 'app_logs';
+const kApp = 'app';
+const kExtension = 'log';
+
+//Keys for models
+const kId = 'id';
+const kName = 'name';
+const kParentId = 'parentId';
+const kGatewayId = 'gatewayId';
+const kLocationId = 'locationId';
+const kSensorId = 'sensorId';
+const kSensorType = 'sensorType';
+const kStatus = 'status';
+
+//Keys Assets status
+const kAlert = 'alert';
+const kOperating = 'operating';
